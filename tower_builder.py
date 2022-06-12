d0a516134bef875fdab208e700914062de0b3d99
Robison Torres <robison.torres21@gmail.com>
2022-06-12 11:27:04 -0300

    It corrects the outputs that should appear.

--- tower_builder.py
+++ tower_builder.py
@@ -3,11 +3,12 @@ print()
 
 def tower_builder(n_floors):
 
-    # This function builds a tower of asterisks.
+    # This function builds a tower of asterisks based on any
+    # given number (number of floors).
     new = ['*'.center(2 * n_floors - 1)]
     while len(new) != n_floors:
         new.append(('**' + (new[-1]).strip()).center(2 * n_floors - 1))
-    return new
+    return '\n'.join(new)
 
 print(tower_builder(5))  # Outputs:
 
