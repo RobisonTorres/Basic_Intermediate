d35c15f465906b4cc97728818ef07fbd705dbe5e
Robison Torres <robison.torres21@gmail.com>
2022-07-20 12:38:21 -0300

    Initial commit.

--- validBraces.py
+++ validBraces.py
@@ -1,9 +1,15 @@
 print('From Code Wars.')
 print()
 
-def validBraces(string):
+def validBraces(s):
 
-    # This function...
-    pass
+    # This function determines if the order of parentheses,
+    # brackets and curly braces is valid.
+    while '{}' in s or '()' in s or '[]' in s:
+        s = s.replace('{}', '')
+        s = s.replace('[]', '')
+        s = s.replace('()', '')
+    return s == ''
 
-print(validBraces('(){}[]'))
+print(validBraces('()()'))
+print(validBraces('[(])'))
